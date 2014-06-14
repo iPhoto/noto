cdd4573b1995d29c59ecde9191b9661d638d9bdb
Daniel Suo <danielsuo@gmail.com>
2014-06-14 18:24:04 -0400

    Fixes #22 - Add InAppSettingsKit to automate settings table view; doesn't look the pretties, but that's ok for now. The behavior defaults to user being able to set settings in either Settings app or Selfie app and the two are synced. Not sure how I feel about this, but not terrible.

--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ platform :ios, "7.0"
 
 pod 'mailgun', '~> 1.0.3'
 pod 'AFNetworking', '~> 1.1.0'
+pod 'InAppSettingsKit', '~> 2.1'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking 1
 #define COCOAPODS_VERSION_PATCH_AFNetworking 0
 
+// InAppSettingsKit
+#define COCOAPODS_POD_AVAILABLE_InAppSettingsKit
+#define COCOAPODS_VERSION_MAJOR_InAppSettingsKit 2
+#define COCOAPODS_VERSION_MINOR_InAppSettingsKit 1
+#define COCOAPODS_VERSION_PATCH_InAppSettingsKit 0
+
 // mailgun
 #define COCOAPODS_POD_AVAILABLE_mailgun
 #define COCOAPODS_VERSION_MAJOR_mailgun 1
