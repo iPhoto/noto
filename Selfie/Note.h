3760dd19bfb6e458628dcf1fa8ca2a1c995a7b07
Daniel Suo <danielsuo@gmail.com>
2014-06-30 18:47:30 -0400

    add note

---
+++ Selfie/Note.h
@@ -0,0 +1,13 @@
+//
+//  Note.h
+//  
+//
+//  Created by Daniel Suo on 6/30/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Note : NSObject
+
+@end
---
+++ Selfie/Note.m
@@ -0,0 +1,13 @@
+//
+//  Note.m
+//  
+//
+//  Created by Daniel Suo on 6/30/14.
+//
+//
+
+#import "Note.h"
+
+@implementation Note
+
+@end
