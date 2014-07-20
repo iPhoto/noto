4ef75a0280d7d1d2796d87199bcd85aa21335641
Daniel Suo <danielsuo@gmail.com>
2014-07-20 01:26:47 -0400

    add archive view controller

---
+++ Selfie/Controllers/ArchiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArchiveViewController.h
+//  Selfie
+//
+//  Created by Daniel Suo on 7/20/14.
+//  Copyright (c) 2014 The Leather Apron Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArchiveViewController : UIViewController
+
+@end
